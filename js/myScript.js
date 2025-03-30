bfda402832f9e8d8b4a0911d01615e5ad88418df
kamnyl <kamillanylund@gmail.com>
2025-03-30 10:24:55 +0200

    Add validateForm function and finalize styling adjustments

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,4 @@
+// Function to validate password
 function isValidPassword(username, password) {
     const passwordRegex = /^(?!.*\s).{8,}$/; // Regex no spaces and minumum 8 characters
 
@@ -10,4 +11,25 @@ function isValidPassword(username, password) {
     }
 
     return true;
+}
+
+// Function to validate the form when submitted
+function validateForm() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
+    const confirmationMessage = document.getElementById("confirmation-message");
+
+    // Call the isValidPassword function to check if password is valid
+    if (!isValidPassword(username, password)) {
+        errorMessage.textContent = "Password must be at least 8 characters long, contain no spaces, and can not include your username.";
+        confirmationMessage.textContent = "";
+        return false; 
+    }
+
+    errorMessage.textContent = "";
+    confirmationMessage.textContent = `Hello ${username.toUpperCase()}, welcome to the club!`;
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    return true; 
 }
